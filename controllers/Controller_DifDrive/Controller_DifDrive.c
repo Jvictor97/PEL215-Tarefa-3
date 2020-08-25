bb58bd2ed383c14c352dfe041b955330b0551d8e
Joao Souza <jvictor.9@hotmail.com>
2020-08-25 00:14:25 -0300

    Created toAngularSpeed function and found L value

--- controllers/Controller_DifDrive/Controller_DifDrive.c
+++ controllers/Controller_DifDrive/Controller_DifDrive.c
@@ -8,14 +8,19 @@
 static WbDeviceTag left_motor, right_motor;
 
 double getVR(double VL, double R) {
-  // const double L = 0.053;
-  double L = 0.115;
+  const double L = 0.0565;
+  // double L = 0.115;
   
   double VR;
   
   VR = VL * (R + L/2) / (R - L/2);
 
   return VR;
+}
+
+double toAngularSpeed(double linearSpeed) {
+  const double r = 0.0205;
+  return linearSpeed / r;
 }
 
 int main(int argc, char **argv)
@@ -31,25 +36,22 @@ int main(int argc, char **argv)
   double VR;  // Velocidade da roda direita (VR)
   double VL;  // Velocidade da roda esquerda (VL)
   double R;  // Raio da circunferencia que o rob� deve fazer
-  double d; // metade da dist�ncia do eixo do rob� em metros
+  // double d; // metade da dist�ncia do eixo do rob� em metros
 
   while (wb_robot_step(TIME_STEP) != -1) {
-
-    /* COLOQUE SEU CODIGO AQUI */
-    VL = 5.0;
-    R = 2.0;
-    
-    printf("R: %.2f, VR: %.2f\n", R, getVR(VL,R));
-    // printf("R: %.2f, VR: %.2f\n", 2.0, getVR(VL, 2.0));
-    // printf("R: %.2f, VR: %.2f\n", 3.0, getVR(VL, 3.0));
-    // printf("R: %.2f, VR: %.2f\n", 4.0, getVR(VL, 4.0));
-    
-    // VR = MAX_SPEED;
-    // VL = MAX_SPEED;
-    
+    VL = 0.05;
+    R = 0.5;
     VR = getVR(VL, R);
-    wb_motor_set_velocity(left_motor, VL);
-    wb_motor_set_velocity(right_motor, VR);
+    
+    // printf("R: %.2f, VR: %.2f\n", R, getVR(VL,R));
+    
+    double wl = toAngularSpeed(VL);
+    double wr = toAngularSpeed(VR);
+    
+    printf("wl: %.5f, wr: %.5f\n", wl, wr);
+        
+    wb_motor_set_velocity(left_motor, wl);
+    wb_motor_set_velocity(right_motor, wr);
 
   };
   wb_robot_cleanup();
